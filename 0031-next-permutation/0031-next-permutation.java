020565abfabb00caa8bba2eba3905ddd680fb863
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-07-18 11:10:19 +0530

    Time: 1 ms (26.63%), Space: 42.5 MB (24.83%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length, index = -1;
+        for(int i = n - 2; i >= 0; i--)
+        {
+            if(nums[i] < nums[i + 1])
+            {
+                index = i;
+                break;
+            }
+        }
+        
+        if(index == -1)
+        {
+            Arrays.sort(nums);
+            return;
+        }
+        
+        for(int i = n - 1; i > index; i--)
+        {
+            if(nums[i] > nums[index])
+            {
+                //swap
+                int temp = nums[i];
+                nums[i] = nums[index];
+                nums[index] = temp;
+                break;
+            }
+        }
+        
+        Arrays.sort(nums, index + 1, n);
+    }
+}
