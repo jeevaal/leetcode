802990c50d2d1cffb5845ae66ea7f201a8d91505
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-07-21 13:49:07 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (75.31%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,36 +9,72 @@
  * }
  */
 class Solution {
+    // without creating a node
+//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//         if(list1 == null)
+//             return list2;
+//         if(list2 == null)
+//             return list1;
+//         // considering always l should be smallest
+//         if(list1.val > list2.val)
+//         {
+//             ListNode temp = list1;
+//             list1 = list2;
+//             list2 = temp;
+//         }
+        
+//         ListNode head = list1;
+        
+//         while(list1 != null && list2 != null)
+//         {
+//             ListNode temp1 = null;
+//             while(list1 != null && list1.val <= list2.val)
+//             {
+//                 temp1 = list1;
+//                 list1 = list1.next;
+//             }
+//             temp1.next = list2;
+            
+//             ListNode temp = list1;
+//             list1 = list2;
+//             list2 = temp;
+//         }
+        
+//         return head;
+//     }
+    
+    // with creating a node
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null)
             return list2;
         if(list2 == null)
             return list1;
+        // considering always l should be smallest
         
-        if(list1.val > list2.val)
-        {
-            ListNode temp = list1;
-            list1 = list2;
-            list2 = temp;
-        }
-        
-        ListNode head = list1;
+        ListNode temp = new ListNode(0);
+        ListNode res = temp;
         
         while(list1 != null && list2 != null)
         {
-            ListNode temp1 = null;
-            while(list1 != null && list1.val <= list2.val)
+            if(list1.val < list2.val)
             {
-                temp1 = list1;
+                temp.next = list1;
+                temp = temp.next;
                 list1 = list1.next;
             }
-            temp1.next = list2;
-            
-            ListNode temp = list1;
-            list1 = list2;
-            list2 = temp;
+            else
+            {
+                temp.next = list2;
+                temp = temp.next;
+                list2 = list2.next;
+            }
         }
         
-        return head;
+        if(list1 != null)
+            temp.next = list1;
+        else
+            temp.next = list2;
+        
+        return res.next;
     }
 }
