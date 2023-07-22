324ce1c9d992210ebbfe8877c49a34f5a0914028
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-07-22 23:39:53 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (74.64%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -16,26 +16,59 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         
+//         List<Integer> l = new ArrayList<>();
+        
+//         Stack<TreeNode> s = new Stack<TreeNode>();
+        
+//         while(true)
+//         {
+//             if(root != null)
+//             {
+//                 s.push(root);
+//                 root = root.left;
+//             }
+//             else
+//             {
+//                 if(s.isEmpty())
+//                     break;
+//                 root = s.pop();
+//                 l.add(root.val);
+//                 root = root.right;
+//             }
+//         }
+//         return l;
+        
+        // Morris Inoreder traversal with space complexity of O(1)
         List<Integer> l = new ArrayList<>();
         
-        Stack<TreeNode> s = new Stack<TreeNode>();
-        
-        while(true)
+        while(root != null)
         {
-            if(root != null)
+            if(root.left == null)
             {
-                s.push(root);
-                root = root.left;
-            }
-            else
-            {
-                if(s.isEmpty())
-                    break;
-                root = s.pop();
                 l.add(root.val);
                 root = root.right;
             }
+            else
+            {
+                TreeNode temp = root.left;
+                while(temp.right != null && temp.right != root)
+                    temp = temp.right;
+                
+                if(temp.right == null)
+                {
+                    temp.right = root;
+                    root = root.left;
+                }
+                else
+                {
+                    temp.right = null;
+                    l.add(root.val);
+                    root = root.right;
+                }
+            }
         }
+        
         return l;
+        
     }
 }
