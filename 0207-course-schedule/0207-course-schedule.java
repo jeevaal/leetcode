fd7df98e83e3910ea06ccf1282c31d81ef727020
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-07-25 14:53:24 +0530

    Time: 10 ms (14.42%), Space: 44.3 MB (39.57%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> adjacencyList = new HashMap<>();
+        HashMap<Integer, Integer> indegree = new HashMap<>();
+        List<Integer> topo = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            adjacencyList.put(i, new ArrayList<>());
+            indegree.put(i, 0);
+        }
+
+        for (int[] prerequisite : prerequisites) {
+            int source = prerequisite[1];
+            int destination = prerequisite[0];
+            addEdge(source, destination, adjacencyList, indegree);
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(indegree.get(i) == 0)
+                q.add(i);
+        }
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            topo.add(node);
+            for (int neighbor : adjacencyList.get(node)) {
+                indegree.put(neighbor, indegree.get(neighbor) - 1);
+                if (indegree.get(neighbor) == 0) {
+                    q.add(neighbor);
+                }
+            }
+        }
+
+        return topo.size() == numCourses; 
+    }
+
+    public void addEdge(int source, int destination, HashMap<Integer, List<Integer>> adjacencyList, HashMap<Integer, Integer> indegree) {
+        adjacencyList.get(source).add(destination);
+        indegree.put(destination, indegree.get(destination) + 1);
+    }
+}
+
