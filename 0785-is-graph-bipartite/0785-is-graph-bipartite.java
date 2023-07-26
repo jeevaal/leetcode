4beb359513d4856e3195a92d6057ab42293cba15
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-07-26 19:35:02 +0530

    Time: 1 ms (65.27%), Space: 44 MB (71.73%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -19,17 +19,41 @@ class Solution {
     
     public boolean check(int source, int graph[][], int coloured[])
     {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(source);
+        // using bfs
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(source);
+//         coloured[source] = 0;
+//         while(!q.isEmpty())
+//         {
+//             int node = q.poll();
+//             for(int i : graph[node])
+//             {
+//                 if(coloured[i] == -1)
+//                 {
+//                     q.add(i);
+//                     coloured[i] = 1 - coloured[node];
+//                 }
+//                 else if(coloured[i] == coloured[node])
+//                 {
+//                     return false;
+//                 }
+                    
+//             }
+//         }
+//         return true;
+        
+        // using dfs
+        Stack<Integer> q = new Stack<>();
+        q.push(source);
         coloured[source] = 0;
         while(!q.isEmpty())
         {
-            int node = q.poll();
+            int node = q.pop();
             for(int i : graph[node])
             {
                 if(coloured[i] == -1)
                 {
-                    q.add(i);
+                    q.push(i);
                     coloured[i] = 1 - coloured[node];
                 }
                 else if(coloured[i] == coloured[node])
