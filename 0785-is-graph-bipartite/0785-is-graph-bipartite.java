57273c2f4340cd05131ceaff6a4e8ab1a6984877
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-07-26 19:32:22 +0530

    Time: 1 ms (65.27%), Space: 44 MB (59.19%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        // here given input is adjacencyList
+        // bfs -- instead of visited , use coloured 
+        // fill with 0s and 1s alternatively
+        int n = graph.length;
+        int coloured[] = new int[n];
+        Arrays.fill(coloured, -1);
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(coloured[i] == -1)
+                if(check(i, graph, coloured) == false)
+                    return false;
+        }
+        
+        return true;
+    }
+    
+    public boolean check(int source, int graph[][], int coloured[])
+    {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(source);
+        coloured[source] = 0;
+        while(!q.isEmpty())
+        {
+            int node = q.poll();
+            for(int i : graph[node])
+            {
+                if(coloured[i] == -1)
+                {
+                    q.add(i);
+                    coloured[i] = 1 - coloured[node];
+                }
+                else if(coloured[i] == coloured[node])
+                {
+                    return false;
+                }
+                    
+            }
+        }
+        return true;
+    }
+    
+}
