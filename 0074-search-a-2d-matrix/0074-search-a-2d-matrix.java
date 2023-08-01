928d5b24ebb313951c8ae17c073ca17aaccc5e0d
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-08-01 22:51:59 +0530

    Time: 0 ms (100.00%), Space: 41 MB (64.83%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = findRow(matrix, target);
+        if(row!= -1)
+            return binarySearchOverRow(row, matrix, target);
+        return false;
+    }
+    
+    public int findRow(int matrix[][], int target)
+    {
+        int low = 0, high = matrix.length - 1;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            
+            if(matrix[mid][0] <= target && target <= matrix[mid][matrix[0].length - 1])
+                return mid;
+            else if(matrix[mid][0] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+    
+    public boolean binarySearchOverRow(int row, int matrix[][], int target)
+    {
+        int low = 0, high = matrix[row].length - 1;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            
+            if(matrix[row][mid] == target)
+                return true;
+            else if(matrix[row][mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return false;
+    }
+}
