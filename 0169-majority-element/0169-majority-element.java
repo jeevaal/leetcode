590778d4bf22f212211f3845987c0c849c2eabb9
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-08-02 08:25:49 +0530

    Time: 2 ms (64.68%), Space: 49 MB (8.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,14 +1,49 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        for(int i=0;i<nums.length;i++)
+        //better approach
+        // HashMap<Integer,Integer> hm = new HashMap<>();
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(hm.containsKey(nums[i]))
+        //         hm.replace(nums[i],hm.get(nums[i])+1);
+        //     else
+        //         hm.put(nums[i],1);
+        //     if(hm.get(nums[i])>nums.length/2)
+        //         return nums[i];
+        // }
+        // return 0;
+        
+        //optimal approach
+        //moore's voting algorithm
+        
+        int count = 0, n = nums.length;
+        int ele = Integer.MIN_VALUE;
+        
+        for(int i = 0; i < n; i++)
         {
-            if(hm.containsKey(nums[i]))
-                hm.replace(nums[i],hm.get(nums[i])+1);
+            if(count == 0)
+            {
+                count++;
+                ele = nums[i];
+            }
+            else if(ele != nums[i])
+            {
+                count--;
+            }
             else
-                hm.put(nums[i],1);
-            if(hm.get(nums[i])>nums.length/2)
-                return nums[i];
+            {
+                count++;
+            }
+        }
+        
+        count = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(nums[i] == ele)
+                count++;
+            if(count > n / 2)
+                return ele;
         }
         return 0;
     }
