17e457005dd2e5c893fb260659de2f31080b554d
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-08-16 18:47:42 +0530

    Time: 1 ms (100.00%), Space: 41.5 MB (56.41%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,86 +1,45 @@
 class Solution {
-    public int myAtoi(String s) {
-//         int n = s.length();
-//         int i = 0, neg = 0, pos = 0;
-//         long res = 0, int_count = 0;
-//         while(i < n)
-//         {
-//             char c = s.charAt(i);
-//             if(c == '-')
-//                 neg++;
-//             else if(c == '+')
-//                 pos++;
-//             else if(neg > 0 && pos > 0)
-//                 return 0;
-//             else if(c >= '0' && c <= '9')
-//             {
-//                 res = res * 10 + c - '0';
-//                 int_count++;
-//             }
-//             else if(c != ' ')
-//             {
-//                 if(int_count > 0)
-//                     return (int)res;
-                
-//                 if(res > Integer.MAX_VALUE)
-//                     return Integer.MAX_VALUE;
-//                 if(res < Integer.MIN_VALUE)
-//                     return Integer.MIN_VALUE;
-
-//                 return (int)res;
-//             }
-            
-//             i++;
-//         }
-//         if(neg > 0)
-//             res = -res;
-        
-//         if(res > Integer.MAX_VALUE)
-//             return Integer.MAX_VALUE;
-//         if(res < Integer.MIN_VALUE)
-//             return Integer.MIN_VALUE;
-        
-//         return (int)res;
-        
+    public int myAtoi(String s) {        
         int i = 0;
         int n = s.length();
 
-        while (i < n && s.charAt(i) == ' ') { // skipping space characters at the beginning
+        while (i < n && s.charAt(i) == ' ') {
             i++;
         }
 
         int positive = 0;
         int negative = 0;
 
-        if (i<n && s.charAt(i) == '+') {
-            positive++; // number of positive signs at the start in string
+        if (i < n && s.charAt(i) == '+') {
+            positive++;
             i++;
         }
 
-        if (i<n && s.charAt(i) == '-') {
-            negative++; // number of negative signs at the start in string
+        if (i < n && s.charAt(i) == '-') {
+            negative++;
             i++;
+        }
+        
+        if (positive > 0 && negative > 0) {
+            return 0;
         }
 
         double ans = 0;
 
         while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
-            ans = ans * 10 + (s.charAt(i) - '0'); // (s.charAt(i) - '0') is converting character to integer
+            ans = ans * 10 + (s.charAt(i) - '0');
             i++;
         }
 
-        if (negative > 0) { // if negative sign exists
+        if (negative > 0) {
             ans = -ans;
         }
-        if (positive > 0 && negative > 0) { // if both +ve and -ve sign exist, Example: +-12
-            return 0;
-        }
 
-        if (ans > Integer.MAX_VALUE) { // if ans > 2^31 - 1
+        if (ans > Integer.MAX_VALUE) {
             ans = Integer.MAX_VALUE;
         }
 
-        if (ans < Integer.MIN_VALUE) { // if ans < -2^31
+        if (ans < Integer.MIN_VALUE) {
             ans = Integer.MIN_VALUE;
         }
 
