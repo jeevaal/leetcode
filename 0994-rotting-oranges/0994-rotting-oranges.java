a02e96da2aad7347afc40dc8aaa6bb882e1accde
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-08-29 12:38:10 +0530

    Time: 2 ms (87.14%), Space: 41.5 MB (33.69%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -11,50 +11,54 @@ class Pair
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
-        if(grid == null || grid.length == 0) return 0;
-        int rows = grid.length;
-        int cols = grid[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        int count_fresh = 0;
-        //Put the position of all rotten oranges in queue
-        //count the number of fresh oranges
-        for(int i = 0 ; i < rows ; i++) {
-            for(int j = 0 ; j < cols ; j++) {
-                if(grid[i][j] == 2) {
-                    queue.offer(new int[]{i , j});
-                }
-                if(grid[i][j] != 0) {
-                    count_fresh++;
-                }
-            }
-        }
-       
-        if(count_fresh == 0) return 0;
-        int countMin = 0, cnt = 0;
-        int dx[] = {0, 0, 1, -1};
-        int dy[] = {1, -1, 0, 0};
+        int m = grid.length;
+        int n = grid[0].length;
+        int countFresh = 0;
         
-        //bfs starting from initially rotten oranges
-        while(!queue.isEmpty()) {
-            int size = queue.size();
-            cnt += size; 
-            for(int i = 0 ; i < size ; i++) {
-                int[] point = queue.poll();
-                for(int j = 0;j<4;j++) {
-                    int x = point[0] + dx[j];
-                    int y = point[1] + dy[j];
-                    
-                    if(x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || 
-                    grid[x][y] == 2) continue;
-                    
-                    grid[x][y] = 2;
-                    queue.offer(new int[]{x , y});
-                }
-            }
-            if(queue.size() != 0) {
-                countMin++;
+        Queue<Pair> q = new LinkedList<>();
+        
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] == 2)
+                    q.add(new Pair(i, j));
+                else if(grid[i][j] != 0)
+                    countFresh++;
             }
         }
-        return count_fresh == cnt ? countMin : -1;
+        
+        if(countFresh == 0)
+            return 0;
+        int count = 0, t = 0;
+        count -= q.size();
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            count += size;
+            for(int k = 0; k < size; k++)
+            {
+                int drow[] = {0, 0, -1, 1};
+                int dcol[] = {-1, 1, 0, 0};
+                int row = q.peek().row;
+                int col = q.peek().col;
+                q.poll();
+                for(int i = 0; i < 4; i++)
+                {
+                    int nrow = row + drow[i];
+                    int ncol = col + dcol[i];
+
+                    if(nrow < 0 || ncol < 0 || nrow >= m || ncol >= n || grid[nrow][ncol] != 1)
+                        continue;
+
+                    grid[nrow][ncol] = 2;
+                    q.add(new Pair(nrow, ncol));
+                }
+            }
+            if(!q.isEmpty())
+                t++;
+        }
+        
+        return (countFresh == count) ? t : -1;
     }
 }
