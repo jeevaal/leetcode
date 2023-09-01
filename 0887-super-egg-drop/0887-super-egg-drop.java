16d32d1cd95efa0c253ad3931e97572b9fc9486d
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-09-01 22:20:30 +0530

    Time: 10 ms (76.65%), Space: 45.4 MB (55.84%) - LeetHub

--- 0887-super-egg-drop/0887-super-egg-drop.java
+++ 0887-super-egg-drop/0887-super-egg-drop.java
@@ -1,35 +1,41 @@
 class Solution {
     public int superEggDrop(int k, int n) {
-        int[][] memo = new int[k + 1][n + 1];
-        return helper(k, n, memo);
-    }
-    private int helper(int K, int N, int[][] memo) {
-        if (N <= 1) {
-            return N;
-        }
-        if (K == 1) {
-            return N;
-        }
-        if (memo[K][N] > 0) {
-            return memo[K][N];
-        }
-        
-        int low = 1, high = N, result = N;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int left = helper(K - 1, mid - 1, memo);
-            int right = helper(K, N - mid, memo);
-            result = Math.min(result, Math.max(left, right) + 1);
-            if (left == right) {
-                break;
-            } else if (left < right) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        memo[K][N] = result;
-        return result;
+        int dp[][] = new int[k + 1][n + 1];
+        for(int row[] : dp)
+            Arrays.fill(row, -1);
+        return solve(k, n, dp);
     }
     
+    public int solve(int k, int n, int dp[][])
+    {
+        if(n <= 1)
+            return n;
+        if(k == 1)
+            return n;
+        
+        if(dp[k][n] != -1)
+            return dp[k][n];
+        
+        int totalMoves = n; // or we can assign Integer.MAX_VALUE
+        
+        int low = 1, high = n;
+        while(low <= high)
+        {
+            int mid = (low + high) / 2;
+            
+            int eggBreak = solve(k - 1, mid - 1, dp);
+            int dontBreak = solve(k, n - mid, dp);
+            
+            int currentMoves = 1 + Math.max(eggBreak, dontBreak);
+            totalMoves = Math.min(totalMoves, currentMoves);
+            if(eggBreak == dontBreak)
+                break;
+            else if(eggBreak > dontBreak)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        
+        return dp[k][n] = totalMoves;
+    }
 }
