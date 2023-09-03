7b494a39572bbae5d292658700d81364907cd189
jeevaal <116821017+jeevaal@users.noreply.github.com>
2023-09-03 12:17:25 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (20.04%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,18 +1,37 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>();
+        // Stack<Character> st = new Stack<>();
+        // for(char c : s.toCharArray())
+        // {
+        //     if(c == '(')
+        //         st.push(c);
+        //     else
+        //     {
+        //         if(!st.isEmpty() && st.peek() == '(')
+        //             st.pop();
+        //         else
+        //             st.push(c);
+        //     }
+        // }
+        // return st.size();
+        
+        // since it is only one type of bracket, we can use variables
+        
+        int st = 0, close = 0;
         for(char c : s.toCharArray())
         {
             if(c == '(')
-                st.push(c);
+                st++;
             else
             {
-                if(!st.isEmpty() && st.peek() == '(')
-                    st.pop();
+                if(st != 0)
+                    st--;
                 else
-                    st.push(c);
+                    close++;
             }
         }
-        return st.size();
+        
+        return st + close;
+        
     }
 }
